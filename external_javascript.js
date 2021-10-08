8f09fd83c5bcc64aff2f78a8ba52b9bb4272f0b8
Anna <anru7989@colorado.edu>
2021-10-08 12:53:49 -0600

    Project Sekai Start

---
+++ external_javascript.js
@@ -0,0 +1,28 @@
+
+document.getElementById("Kohane").addEventListener("click", Kohane);
+
+function Kohane(){
+  document.getElementById("c1").innerHTML = "This is Kohane Azusawa. She is baby.";
+  document.getElementById("image1").src = "Assets/Koahne.png";
+}
+
+document.getElementById("An").addEventListener("click", An);
+
+function An(){
+  document.getElementById("c1").innerHTML = "Meet An Shiraishi! She is my favorite.";
+    document.getElementById("image1").src = "Assets/An.png";
+}
+
+document.getElementById("Akito").addEventListener("click", Akito);
+
+function Akito(){
+  document.getElementById("c1").innerHTML = "This ginger here is Akito Shinonome. He has daddy issues.";
+    document.getElementById("image1").src = "Assets/Akito.png";
+}
+
+document.getElementById("Toya").addEventListener("click", Toya);
+
+function Toya(){
+  document.getElementById("c1").innerHTML = "Here we have Toya Aoyagi. He's emo.";
+    document.getElementById("image1").src = "Assets/Touya.png";
+}
